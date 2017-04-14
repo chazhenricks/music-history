0da46052597e92ce8b951b3ac983cdf7e89be5c6
Chaz Henricks <chazhenricks@gmail.com>
2017-04-14 07:57:49 -0500

    saving backup

--- js/songs.js
+++ js/songs.js
@@ -3,13 +3,13 @@ var songs = [];
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-//songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-//songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 
 
 //Add one song to the beginning and the end of the array.
-//songs.push("Do You Still Love Me > by Ryan Adams on the album Prisoner");
+songs.push("Do You Still Love Me > by Ryan Adams on the album Prisoner");
 songs.unshift("Typical > by Mutemath on the album Mutemath")
 
 

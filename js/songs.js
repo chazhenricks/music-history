38d52a626ee2d00cf72b2d0bd9ffdc804dac2243
Chaz Henricks <chazhenricks@gmail.com>
2017-05-05 15:31:21 -0500

    rewrote with jquery

--- js/songs.js
+++ js/songs.js
@@ -1,44 +1,28 @@
-//Setting up XHR for music
-var musicRequest = new XMLHttpRequest();
-var musicRequest2 = new XMLHttpRequest();
+var Songs = (function(songlist){
+  var musicHTML = "";
 
-musicRequest.open("GET", "music.json");
-musicRequest2.open("GET", "music2.json");
-
-musicRequest.send();
-musicRequest2.send();
-
-
-//declaring event listeners for XHR
-musicRequest.addEventListener("load", getMusic);
-musicRequest2.addEventListener("load", getMusic2);
-
-//declaring variable to store parsed music JSON file
-var musicList = "";
-var musicList2 ="";
-
-
-//variable for HTML elements to be written to page
-var musicHTML = "";
-var moreBtn = "";
-
-//declaring HTML element for musicList to go
-var songList = document.getElementById("song-list");
-
-//getMusic function that runs when page loads
-function getMusic(event){
-    musicList = JSON.parse(this.responseText);
-    populateDOM();
+// Ajax call to grab songs from first JSON
+  songlist.ajaxCall1 = function(){
+    $.ajax({
+      url : "music.json"
+    })
+    .done(songlist.populateDOM).done(songlist.ajaxCall2);
   };
 
-  function getMusic2(event){
-    musicList2 = JSON.parse(this.responseText);
+//Ajax call to grab songs from second JSON
+  songlist.ajaxCall2 = function(){
+    $.ajax({
+      url : "music2.json"
+    }).done(songlist.populateDOM).done(songlist.eventListeners);
+  };
 
-  }
+// Populates DOM with item from AJAX call
+  songlist.populateDOM = function(musicList){
+    console.log("musicList", musicList)
 
-//Stores original music.json info to musicHTML var and writes to page.
-function populateDOM(){
-    for (var i=0;i<musicList.music.length;i++){
+    musicHTML = "";
+
+  for (var i=0;i<musicList.music.length;i++){
         musicHTML +=
          ` <section>
                   <header>
@@ -53,109 +37,84 @@ function populateDOM(){
             </section>`
     }
 
-  musicHTML += `<button id="more">More</button>`;
-  songList.innerHTML = musicHTML;
+  $("#song-list").append(musicHTML);
+  //calls function to add click events to delete buttons
+  songlist.addDelete();
 
-  //runs the function to add event listeners to all of the delete buttons
-  addDelete();
+};
 
-//creates a "more" button and adds event listener to show music2 JSON file
-  moreBtn = document.getElementById("more");
-  moreBtn.addEventListener("click", function(event){
-    populateDOM2();
+
+//adds click events to delete buttons
+songlist.addDelete = function(){
+
+  $(".delete").click(function(){
+     var getOuttaHere = event.target.closest("section");
+      console.log("getOuttaHere", getOuttaHere);
+      getOuttaHere.remove(getOuttaHere);
   });
 };
 
-//function that adds info from music2 json file to var and adds to page
-function populateDOM2 (){
-  var musicHTML2 = "";
-  for (var i=0;i<musicList2.music.length;i++){
-         musicHTML2 +=
-         ` <section>
-                  <header>
-                    <h2>${musicList2.music[i].song}</h2>
-                  </header>
-                  <ul>
-                    <li>${musicList2.music[i].artist}</li>
-                    <li>${musicList2.music[i].album}</li>
-                    <li>${musicList2.music[i].genre}</li>
-                  <li><button class="delete">X</button></li>
-                  </ul>
-            </section>`
-    }
+songlist.eventListeners = function(){
+  var addMusicPage = $("#add-music");
+  var songListPage = $("#song-list");
+  var filtersPage = $("#filters");
 
-  //replaces DOM with original music.json info as well as the newly appended music2.json info
-  songList.innerHTML = musicHTML;
-  songList.innerHTML += musicHTML2;
-  addDelete();
-};
+  $("#view-music-button").click(function(){
+    addMusicPage.addClass("hidden");
+    songListPage.removeClass("hidden");
+    filtersPage.removeClass("hidden");
+  });
 
-//Add event listeners to delete buttons to remove
-function addDelete(){
-  var deleteBtns = document.getElementsByClassName("delete");
-  console.log("deleteBtns", deleteBtns);
-  for (var i=0;i<deleteBtns.length;i++){
-      deleteBtns.item(i).addEventListener("click", function(event){
-        var getOuttaHere = event.target.closest("section");
-        console.log("getOuttaHere", getOuttaHere);
-        getOuttaHere.remove(getOuttaHere);
-      });
-  }
-}
+  $("#add-music-button").click(function(){
+    addMusicPage.removeClass("hidden");
+    songListPage.addClass("hidden");
+    filtersPage.addClass("hidden");
+  });
 
-
-//Addevent listener to hide and unhide the add music page and filters and
-
-var viewMusicButton = document.getElementById("view-music-button");
-var addMusicButton = document.getElementById("add-music-button");
-
-var addMusicPage = document.getElementById("add-music");
-var musicFilterPage = document.getElementById("filters");
-var songsListPage = document.getElementById("song-list");
-
-
-addMusicButton.addEventListener("click", showAddMusic);
-viewMusicButton.addEventListener("click", hideAddMusic);
-
-function showAddMusic(event){
-  addMusicPage.classList.remove("hidden");
-  songsListPage.classList.add("hidden")
-  musicFilterPage.classList.add("hidden")
-
-};
-
-function hideAddMusic(event){
-  addMusicPage.classList.add("hidden")
-  songsListPage.classList.remove("hidden");
-  musicFilterPage.classList.remove("hidden");
-
+  $("#add-button").click(function(){
+    var newSong = {
+      "music":[{
+      "artist": $("#add-artist").val(),
+      "song": $("#add-song").val(),
+      "album": $("#add-album").val(),
+      "genre": $("#add-genre").val()
+    }]};
+    console.log("newSong", newSong);
+    songlist.populateDOM(newSong);
+  });
 
 };
 
 
 
+  return songlist;
+
+})(Songs || {})
+
+Songs.ajaxCall1();
 
 
 
-//Add button on Add Music page pushed another item to music2.json file
 
-var addBtn = document.getElementById("add-button");
+// //Add button on Add Music page pushed another item to music2.json file
 
-addBtn.addEventListener("click", function(event){
-  var newArtist = document.getElementById("add-artist");
-  var newSong = document.getElementById("add-song");
-  var newAlbum = document.getElementById("add-song");
-  var newGenre = document.getElementById("add-genre")
+// var addBtn = document.getElementById("add-button");
 
-  musicList2.music.push(
-    {
-      "artist": newArtist.value,
-      "song": newSong.value,
-      "album": newAlbum.value,
-      "genre": newGenre.value
-    });
-  populateDOM2();
-});
+// addBtn.addEventListener("click", function(event){
+//   var newArtist = document.getElementById("add-artist");
+//   var newSong = document.getElementById("add-song");
+//   var newAlbum = document.getElementById("add-song");
+//   var newGenre = document.getElementById("add-genre")
+
+//   musicList2.music.push(
+//     {
+//       "artist": newArtist.value,
+//       "song": newSong.value,
+//       "album": newAlbum.value,
+//       "genre": newGenre.value
+//     });
+//   populateDOM2();
+// });
 
 
 
